5523b30d75a0a1db4f435d7fe7b0d359704974eb
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-08-16 15:25:47 +0530

    Updated

--- 4-javascript/hello.js
+++ 4-javascript/hello.js
@@ -8,3 +8,19 @@ const num = [1,2,3,4]
 console.log(num);
 num[0]=9
 console.log(num);
+
+const person = {
+    firstName:"John",
+    lastName:"King",
+    gender:"male",
+    age:34,
+    role:"developer",
+
+    fullName:function demo(){
+        return this.firstName+" "+this.lastName;
+    }
+};
+
+person.name="King"
+
+console.log(person);
--- 4-javascript/operators.js
+++ 4-javascript/operators.js
@@ -1,3 +1,8 @@
 carName = "Audi";
 var carName
 console.log(carName);
+
+let str1 = new String("hello")
+let str2 = new String("hello")
+
+console.log(str1==str2);
